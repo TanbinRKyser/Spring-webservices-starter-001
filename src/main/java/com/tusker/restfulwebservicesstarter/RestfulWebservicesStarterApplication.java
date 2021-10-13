46214ceede10b8043720eecc6544442acd340340
foxcatcher24 <tmitul515@gmail.com>
2021-10-13 13:02:57 +0600

    Initial Commit

---
+++ src/main/java/com/tusker/restfulwebservicesstarter/RestfulWebservicesStarterApplication.java
@@ -0,0 +1,13 @@
+package com.tusker.restfulwebservicesstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebservicesStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tusker/restfulwebservicesstarter/RestfulWebservicesStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusker.restfulwebservicesstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
