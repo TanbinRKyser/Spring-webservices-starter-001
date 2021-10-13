231cf89e4beaeaebc5f7aa4fb8930d5da10f1568
foxcatcher24 <tmitul515@gmail.com>
2021-10-13 14:07:17 +0600

    Added a hello world service.

--- src/main/java/com/tusker/restfulwebservicesstarter/controller/HelloWorldController.java
+++ src/main/java/com/tusker/restfulwebservicesstarter/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.tusker.restfulwebservicesstarter.controller;
 
+import com.tusker.restfulwebservicesstarter.service.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,5 +12,10 @@ public class HelloWorldController {
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
     }
 }
