efab1396c0cddd3cb050b872b239387a4547a53c
foxcatcher24 <tmitul515@gmail.com>
2021-10-16 19:19:19 +0600

    Tweaking Swagger Documentation.

--- src/main/java/com/tusker/restfulwebservicesstarter/config/SwaggerConfig.java
+++ src/main/java/com/tusker/restfulwebservicesstarter/config/SwaggerConfig.java
@@ -11,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
@@ -25,13 +26,13 @@ public class SwaggerConfig {
     );
 
     public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
-        "Awesome API Title",
+        "Tuskers API Title",
         "Awesome API Description",
         "1.0",
         "urn:tos", DEFAULT_CONTACT,
         "Apache 2.0",
         "https://www.apache.org/licenses/LICENSE-2.0",
-        Arrays.asList()
+        List.of()
     );
 
     private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
@@ -41,7 +42,9 @@ public class SwaggerConfig {
     @Bean
     public Docket api(){
         return new Docket( DocumentationType.SWAGGER_2 )
-                .apiInfo( DEFAULT_API_INFO );
+                .apiInfo( DEFAULT_API_INFO )
+                .produces( DEFAULT_PRODUCES_AND_CONSUMES )
+                .consumes( DEFAULT_PRODUCES_AND_CONSUMES );
     }
 
 }
--- src/main/java/com/tusker/restfulwebservicesstarter/model/User.java
+++ src/main/java/com/tusker/restfulwebservicesstarter/model/User.java
@@ -1,14 +1,23 @@
 package com.tusker.restfulwebservicesstarter.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel( description = "API MODEL -- << No Useful description >>")
 public class User {
+
     private int id;
+
     @Size(min = 2, message = "Name should've at least 2 characters")
+    @ApiModelProperty( notes = "Name can't be smaller than 2 char")
     private String name;
+
     @Past
+    @ApiModelProperty( notes = "Birthday can't be in the future")
     private Date birthDate;
 
     public User() {
